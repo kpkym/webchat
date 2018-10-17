761d90a31a526660c4a729d9b16caa5d6b278c81
kpkym <okpkym@gmail.com>
2018-10-17 22:53:32 +0800

    spring-boot

---
+++ src/main/java/com/ou/WebcatApplication.java
@@ -0,0 +1,19 @@
+package com.ou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class WebcatApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebcatApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebcatApplication.class, args);
+    }
+}
--- src/main/webapp/component/bootjs_jquery.jsp
+++
@@ -1,8 +0,0 @@
-<script src="${APP_PATH}/static/jquery/jquery-3.3.1.js"></script>
-<script src="${APP_PATH}/static/bootstrap-4.1.3/js/bootstrap.bundle.js"></script>
-<script src="${APP_PATH}/static/bootstrap-4.1.3/js/popper.min.js"></script>
-<script src="${APP_PATH}/static/bootstrap-4.1.3/js/bootstrap.js"></script>
-<script src="${APP_PATH}/static/layui/layui.all.js"></script>
-<script>
-    APP_PATH = "${APP_PATH}";
-</script>
